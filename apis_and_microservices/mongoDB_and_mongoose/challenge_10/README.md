a65229af7b9cea0a3f9088b9ff0dd5870d36d817
Taise Soares <taise@pulsup.com.br>
2020-12-02 07:57:31 -0300

    Delete One Document Using model.findByIdAndRemove
    Signed-off-by: Taise Soares <taise@pulsup.com.br>

---
+++ apis_and_microservices/mongoDB_and_mongoose/challenge_10/README.md
@@ -0,0 +1,12 @@
+## 10º Desafio
+
+findByIdAndRemovee findOneAndRemovesão como os métodos de atualização anteriores. Eles passam o documento removido para o banco de dados. Como de costume, use o argumento da função personIdcomo chave de pesquisa.
+
+
+<hr>
+<br>
+
+## 10º Challenge
+
+findByIdAndRemove and findOneAndRemove are like the previous update methods. They pass the removed document to the db. As usual, use the function argument personId as the search key.
+
---
+++ apis_and_microservices/mongoDB_and_mongoose/challenge_10/myApp.js
@@ -0,0 +1,119 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGO_URI)
+
+const Schema = mongoose.Schema
+
+let Person;
+
+
+const personSchema = new Schema({
+  name: { type: String, required: true },
+  age: Number,
+  favoriteFoods: [String]
+})
+
+Person = mongoose.model("Person", personSchema);
+
+const createAndSavePerson = (done) => {
+  const newPerson = new Person({ name: 'Taise', age: 30, favoriteFoods: ['Mexican', 'Pizza'] })
+
+  newPerson.save(function(err, data) {
+    if (err) return console.error(err)
+    done(null, data);
+  })
+};
+
+let arrayOfPeople = [
+  { name: 'Taise', age: 30, favoriteFoods: ['Mexican', 'Pizza'] },
+  { name: 'Jessica', age: 28, favoriteFoods: ['Japanese', 'Pizza'] },
+  { name: 'Arthur', age: 5, favoriteFoods: ['Cookies', 'snacks'] }
+]
+
+const createManyPeople = (arrayOfPeople, done) => {
+  Person.create(arrayOfPeople, function(err, data) {
+    if (err) return console.error(err)
+    done(null, data);
+  })
+};
+
+const findPeopleByName = (personName, done) => {
+  Person.find({ name: personName }, function(err, data) {
+    if (err) return console.error(err)
+    done(null, data);
+  })
+};
+
+const findOneByFood = (food, done) => {
+  Person.findOne({ favoriteFoods: food }, function(err, data) {
+    if (err) return console.error(err)
+    done(null, data);
+  })
+};
+
+const findPersonById = (personId, done) => {
+  Person.findById({ _id: personId }, function(err, data) {
+    if (err) return console.error(err)
+    done(null, data);
+  })
+};
+
+const findEditThenSave = (personId, done) => {
+  const foodToAdd = "hamburger";
+  Person.findById({ _id: personId }, function(err, person) {
+    if (err) return console.error(err)
+
+    person.favoriteFoods.push(foodToAdd)
+
+    person.save((err, data) => {
+      if (err) return console.error(err)
+      done(null, data);
+    })
+  })
+};
+
+const findAndUpdate = (personName, done) => {
+  const ageToSet = 20;
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, { new: true}, (err, person) => {
+    if (err) return console.error(err)
+    done(null, person);
+  })
+};
+
+const removeById = (personId, done) => {
+  Person.findOneAndRemove({ _id: personId }, (err, data) => {
+    if (err) return console.error(err)
+    done(null, data);
+  })
+};
+
+const removeManyPeople = (done) => {
+  const nameToRemove = "Mary";
+
+  done(null /*, data*/);
+};
+
+const queryChain = (done) => {
+  const foodToSearch = "burrito";
+
+  done(null /*, data*/);
+};
+
+/** **Well Done !!**
+/* You completed these challenges, let's go celebrate !
+ */
+
+//----- **DO NOT EDIT BELOW THIS LINE** ----------------------------------
+
+exports.PersonModel = Person;
+exports.createAndSavePerson = createAndSavePerson;
+exports.findPeopleByName = findPeopleByName;
+exports.findOneByFood = findOneByFood;
+exports.findPersonById = findPersonById;
+exports.findEditThenSave = findEditThenSave;
+exports.findAndUpdate = findAndUpdate;
+exports.createManyPeople = createManyPeople;
+exports.removeById = removeById;
+exports.removeManyPeople = removeManyPeople;
+exports.queryChain = queryChain;
